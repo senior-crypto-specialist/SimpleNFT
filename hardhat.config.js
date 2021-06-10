fb34a3d7494b3d89230d6adc707d6f953ffb69e3
fullstackdev215 <genius.developer.63@gmail.com>
2021-06-10 11:04:45 +0300

    implement mainnet fork config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,23 +1,46 @@
-require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("solidity-coverage");
+require("@nomiclabs/hardhat-etherscan");
 
-// Go to https://www.alchemyapi.io, sign up, create
-// a new App in its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY = "oojMfZkYjgc64EeNl3uQCgnesgLiojvu";
+require("dotenv").config();
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 
-// Replace this private key with your Ropsten account private key
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Be aware of NEVER putting real Ether into testing accounts
-const ROPSTEN_PRIVATE_KEY = "8ffe3784a1b9c786a07ab1625a086dfd4d6942b49a63d2a805f46aaded8ed991";
+const { utils } = require("ethers");
 
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
+  defaultNetwork: "hardhat",
   solidity: "0.7.3",
   networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+        blockNumber: 12441069,
+      },
+      blockGasLimit: 12000000,
+    },
+    mainnet: {
+      url: `https://eth.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+      timeout: 150000,
+      gasPrice: parseInt(utils.parseUnits("25", "gwei"))
+    },
+    kovan: {
+      url: `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
     ropsten: {
       url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+      accounts: [`0x${PRIVATE_KEY}`]
     }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN
   }
 };
 
